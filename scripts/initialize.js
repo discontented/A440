f1bad6746b91cb918014ceb5b95a0709cb04e1ae
Josh Corneille <jcorneille107@gmail.com>
2018-03-22 13:28:11 -0400

    corrected mistargeted link of init.js

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,4 +1,4 @@
 $(function() {
-    $("#header").load("A440/htmlAssets/header.html");
-    $("#footer").load("A440/htmlAssets/footer.html");
+    $("#header").load("htmlAssets/header.html");
+    $("#footer").load("htmlAssets/footer.html");
 })
