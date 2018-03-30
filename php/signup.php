d08e3766889b0c7b5f69d175cbbfa1b9c7acd322
njfleck <nathanjfleck@hotmail.com>
2018-03-30 16:50:22 -0400

    fixed button
    now adds user to Server

--- php/signup.php
+++ php/signup.php
@@ -3,9 +3,10 @@
 //if (isset($_POST['submit'])){
     include_once 'mySqlLogin.php';
     $userName = $_POST['username'];
-    $password = $_POST['passwd'];
+    //$password = $_POST['passwd'];
+    $true = true; 
 
-    $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$password');";
+    $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
     mysqli_query($conn, $sql); 
 
     echo($user);
