99740be11051217ce153c6899bd77029a47392eb
njfleck <nathanjfleck@hotmail.com>
2018-04-02 18:22:20 -0400

    updated host login
    changed it to see if host is already a participant

---
+++ php/hostLogin.php
@@ -0,0 +1,19 @@
+<?php
+
+    include_once 'mySqlLogin.php';
+    $userName = $_POST['username'];
+    $true = true; 
+    $sql = "SELECT * FROM Participant WHERE username='$userName'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0 ){
+        //throws ERROR, User already logged in
+        header("Location: ../hostLogin.php?signup=usertaken");
+        exit;
+    } else {
+        $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
+        mysqli_query($conn, $sql); 
+
+        echo($user);
+    }
+    
+   
