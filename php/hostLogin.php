e04e31333a1ad18197a618f78f77aae74b0801ea
njfleck <nathanjfleck@hotmail.com>
2018-04-02 18:50:02 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -5,14 +5,14 @@
     $true = true; 
     $sql = "SELECT * FROM Participant WHERE username='$userName'";
     $result = mysqli_query($conn, $sql);
-    if(mysqli_num_rows($result) > 0 ){
+    //if(mysqli_num_rows($result) > 0 ){
         //throws ERROR, User already logged in
         //header("Location: /index.html?signup=usertaken");
-        exit;
-    } else {
+    //    exit;
+    //} else {
         $sql1 = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
         mysqli_query($conn, $sql1); 
         echo($user);
-    }
+    //}
     
    
