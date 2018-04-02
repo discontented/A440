0ee513a54b1b59c92924bfc94e0fa83fa7bc293a
njfleck <nathanjfleck@hotmail.com>
2018-04-02 18:54:13 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -3,8 +3,8 @@
     include_once 'mySqlLogin.php';
     $userName = $_POST['username'];
     $true = true; 
-    $sql = "SELECT * FROM Participant WHERE username='$userName'";
-    $result = mysqli_query($conn, $sql);
+    //$sql = "SELECT * FROM Participant WHERE username='$userName'";
+    //$result = mysqli_query($conn, $sql);
     //if(mysqli_num_rows($result) > 0 ){
         //throws ERROR, User already logged in
         //header("Location: /index.html?signup=usertaken");
