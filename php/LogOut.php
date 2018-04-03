76dfa78636dfb80c3d7a1bcd05d081d0bfa5c807
njfleck <nathanjfleck@hotmail.com>
2018-04-03 01:30:34 -0400

    test
    test

--- php/LogOut.php
+++ php/LogOut.php
@@ -7,16 +7,11 @@ $GuestID = $_SESSION['UserID'];
 $SessionID_Get =$_SESSION['SessionID'] ;
 
 
-$sql_select = "SELECT * FROM Participant WHERE username='$userName'";
-$result = mysqli_query($conn, $sql_select);
-$row = mysqli_fetch_assoc($result);
-$IsHost= $row['host'];
-
 $sql = "DELETE FROM `Session_Guest` WHERE `Session_Guest`.`UserID` = $GuestID AND `Session_Guest`.`SessionID` = $SessionID_Get;";
 mysqli_query($conn, $sql);
 $sql = "DELETE FROM `Participant` WHERE `Participant`.`UserID` = $GuestID;";
 mysqli_query($conn, $sql);
-if($IsHost==1){
+if($_SESSION['host']){
     $sql = "DELETE FROM `Room` WHERE `Room`.`SessionID` = $SessionID_Get;";
     mysqli_query($conn, $sql);
 } else {
