8331c872d29530ff853634caaeda2ee4f4b25a11
njfleck <nathanjfleck@hotmail.com>
2018-04-02 21:06:37 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -13,14 +13,14 @@
     //} else {
         $sql_Participant = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
         mysqli_query($conn, $sql_Participant); 
-        $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
-        $result = mysqli_query($conn, $sql_select);
-        $row = mysqli_fetch_assoc($result);
-        echo($user);
-        $sql_Room = "INSERT INTO Room (time_stamp) VALUES ('NULL');";
-        mysqli_query($conn, $sql_Room); 
-        $sql_Connector = "INSERT INTO Session_Guest (User_ID, Session_ID) VALUES ($row['User_ID']','0'');";
-        mysqli_query($conn, $sql_Connector); 
+        // $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
+        // $result = mysqli_query($conn, $sql_select);
+        // $row = mysqli_fetch_assoc($result);
+        // echo($user);
+        // $sql_Room = "INSERT INTO Room (time_stamp) VALUES ('NULL');";
+        // mysqli_query($conn, $sql_Room); 
+        // $sql_Connector = "INSERT INTO Session_Guest (User_ID, Session_ID) VALUES ($row['User_ID']','0'');";
+        // mysqli_query($conn, $sql_Connector); 
     //}
     
     ?>
