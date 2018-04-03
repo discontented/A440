d62d7c8db3fa0d34e293b9cb931cf518d64e582a
njfleck <nathanjfleck@hotmail.com>
2018-04-02 22:36:59 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -10,13 +10,14 @@
     mysqli_query($conn, $sql_Room); 
 
 
-    sleep(2);
-    $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
-    $result = mysqli_query($conn, $sql_select);
-    $row = mysqli_fetch_assoc($result);
+    //$_SESSION[]= $row['']
+    // sleep(2);
+    // $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
+    // $result = mysqli_query($conn, $sql_select);
+    // $row = mysqli_fetch_assoc($result);
 
-    $sql_Connector = "INSERT INTO Session_Guest (UserID) VALUES ('1');";
-    mysqli_query($conn, $sql_Connector); 
+    // $sql_Connector = "INSERT INTO Session_Guest (UserID) VALUES ('1');";
+    // mysqli_query($conn, $sql_Connector); 
     
     
     ?>
