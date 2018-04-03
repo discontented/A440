06a6631de614f32f40088df1477c96f0e6b34733
njfleck <nathanjfleck@hotmail.com>
2018-04-03 01:01:38 -0400

    test
    test

--- php/LogOut.php
+++ php/LogOut.php
@@ -1,11 +1,11 @@
 <?php
 
 session_start();
-
-$sql = "DELETE FROM Participant WHERE UserID = '$_SESSION['SessionID']'";
-$result = mysqli_query($conn, $sql);
 $sql = "DELETE FROM Session_Guest WHERE UserID = '$_SESSION['SessionID']'";
 
 $result = mysqli_query($conn, $sql);
+$sql = "DELETE FROM Participant WHERE UserID = '$_SESSION['SessionID']'";
+$result = mysqli_query($conn, $sql);
+
 session_destroy();
 ?>
