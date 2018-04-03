175d74ec5eae75e71957601cafd09a15920244b4
njfleck <nathanjfleck@hotmail.com>
2018-04-02 20:07:35 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -1,21 +1,21 @@
 <?php
-      //header("Location: main.html");
+    
     include_once 'mySqlLogin.php';
     $userName = $_POST['username'];
     $true = true; 
-    //$sql_select = "SELECT * FROM Participant";
-    //$result = mysqli_query($conn, $sql_select);
+    $sql_select = "SELECT * FROM Participant";
+    $result = mysqli_query($conn, $sql_select);
     //echo($result);
-    //if(mysqli_num_rows($result) > 0 ){
+    if(mysqli_num_rows($result) > 0 ){
         //throws ERROR, User already logged in
-        //header("Location: /index.html?signup=usertaken");
-    //    exit;
-    //} else {
+        header("Location: /index.html?signup=usertaken");
+        exit;
+    } else {
         $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
         mysqli_query($conn, $sql); 
         echo($user);
       
-    //}
+    }
     ?>
     
    
