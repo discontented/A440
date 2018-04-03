4b489baf5aca7c9287abf0c7b0b6cddee43f811d
njfleck <nathanjfleck@hotmail.com>
2018-04-03 01:12:20 -0400

    test
    test

--- php/LogOut.php
+++ php/LogOut.php
@@ -4,13 +4,12 @@ session_start();
 
 include_once 'mySqlLogin.php';
 $GuestID = $_SESSION['SessionID'];
-//$sql = "DELETE FROM Session_Guest WHERE UserID = '$GuestID'";
+$sql = "DELETE FROM Session_Guest WHERE UserID = '$GuestID'";
 
-//$result = mysqli_query($conn, $sql);
-//$sql = "DELETE FROM Participant WHERE UserID = '$GuestID'";
-//$result = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
+$sql = "DELETE FROM Participant WHERE UserID = '$GuestID'";
+$result = mysqli_query($conn, $sql);
 
-$sql_Participant = "INSERT INTO Participant (username, host) VALUES ('$GuestID', 'true');";
-    mysqli_query($conn, $sql_Participant); 
+
 session_destroy();
 ?>
