05fcd2b179f2137d164f8619bc41b3448db549fe
njfleck <nathanjfleck@hotmail.com>
2018-04-02 21:04:48 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -11,16 +11,16 @@
        // echo("Your username is already take");
         //exit;
     //} else {
-        $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
-        mysqli_query($conn, $sql); 
+        $sql_Participant = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
+        mysqli_query($conn, $sql_Participant); 
         $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
         $result = mysqli_query($conn, $sql_select);
         $row = mysqli_fetch_assoc($result);
-        //echo($user);
-        $sql = "INSERT INTO Room (time_stamp) VALUES ('NULL');";
-        mysqli_query($conn, $sql); 
-        $sql = "INSERT INTO Session_Guest (User_ID, Session_ID) VALUES ($row['User_ID']','0'');";
-        mysqli_query($conn, $sql); 
+        echo($user);
+        $sql_Room = "INSERT INTO Room (time_stamp) VALUES ('NULL');";
+        mysqli_query($conn, $sql_Room); 
+        $sql_Connector = "INSERT INTO Session_Guest (User_ID, Session_ID) VALUES ($row['User_ID']','0'');";
+        mysqli_query($conn, $sql_Connector); 
     //}
     
     ?>
