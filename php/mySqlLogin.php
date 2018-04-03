f8d03472f345e712509c07d12a64bb0eb8568163
Josh Corneille <jcorneille107@gmail.com>
2018-04-03 02:05:11 -0400

    fixed

--- php/mySqlLogin.php
+++ php/mySqlLogin.php
@@ -2,8 +2,8 @@
 session_start();
 $db_hostname = 'localhost';
 $db_database = 'A440';
-$db_username = 'www';
-$db_password = 'guest';
+$db_username = 'root';
+$db_password = 'harmony';
 
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 
