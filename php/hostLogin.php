d22bdab98083226ddaf6534cfe45a1bd3e1df0e0
njfleck <nathanjfleck@hotmail.com>
2018-04-02 20:00:14 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -1,18 +1,19 @@
 <?php
-      header("Location: main.html");
-    //include_once 'mySqlLogin.php';
-    //$userName = $_POST['username'];
-    //$true = true; 
-    //$sql = "SELECT * FROM Participant WHERE username='$userName'";
-    //$result = mysqli_query($conn, $sql);
+      //header("Location: main.html");
+    include_once 'mySqlLogin.php';
+    $userName = $_POST['username'];
+    $true = true; 
+    $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
+    $result = mysqli_query($conn, $sql_select);
+    echo($result);
     //if(mysqli_num_rows($result) > 0 ){
         //throws ERROR, User already logged in
         //header("Location: /index.html?signup=usertaken");
     //    exit;
     //} else {
-       // $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
-        //$result = mysqli_query($conn, $sql); 
-        //echo($user);
+        $sql = "INSERT INTO Participant (username, host) VALUES ('$userName', '$true');";
+        mysqli_query($conn, $sql); 
+        echo($user);
       
     //}
     ?>
