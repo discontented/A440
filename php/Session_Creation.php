75807f7c77d98492195f46a2f121786ffdd55a75
njfleck <nathanjfleck@hotmail.com>
2018-04-02 23:06:51 -0400

    test
    test

--- php/Session_Creation.php
+++ php/Session_Creation.php
@@ -1,10 +1,11 @@
 <?php
 
-
-    $sql_select_id = "SELECT * FROM Room WHERE id=(SELECT MAX(id) FROM Room);";
+    $userName = $_POST['username'];
+    $sql_select_id = "SELECT * FROM Room WHERE SessionID=(SELECT MAX(SessionID) FROM Room);";
     $result_id = mysqli_query($conn, $sql_select_id);
     $row_id = mysqli_fetch_assoc($result_id);
     $_SESSION['id']= $row_id['SessionID'];
+
 
     $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
     $result = mysqli_query($conn, $sql_select);
