97086925f5d4bf34a4d5aec9388b678572f67d10
Josh Corneille <jcorneille107@gmail.com>
2018-04-03 03:32:53 -0400

    tired

--- php/selectSong.php
+++ php/selectSong.php
@@ -11,7 +11,7 @@ $selection = new Song($db, $_POST['selection']);
 $sessionPlaylist = new Playlist($db);
 
 //Adds the song by songID to the current playlist
-$sessionPlaylist->addSong($selection->songID());
+$sessionPlaylist->addSong($selection->songID);
 
 $keys = ['song_id', 'track_name', 'votes'];
 $values = [$selection->songID, $selection->title, $selection->numVotes];
