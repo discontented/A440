87166fc84593bc4e2743f29060736a656d358495
njfleck <nathanjfleck@hotmail.com>
2018-04-02 23:01:35 -0400

    test
    test

--- php/Session_Creation.php
+++ php/Session_Creation.php
@@ -10,7 +10,7 @@
     $result = mysqli_query($conn, $sql_select);
     $row = mysqli_fetch_assoc($result);
 
-    $sql_Connector = "INSERT INTO Session_Guest (UserID, SessionID) VALUES ('$row['UserID'] ,'$_SESSION['id']');";
+    $sql_Connector = "INSERT INTO Session_Guest (UserID, SessionID) VALUES ('$row['UserID']'  ,'$_SESSION['id']');";
     mysqli_query($conn, $sql_Connector); 
 
 
