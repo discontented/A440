1149550a4399e2ef1d57fed25e06a39c200b662d
njfleck <nathanjfleck@hotmail.com>
2018-04-02 23:05:06 -0400

    test
    test

--- php/Session_Creation.php
+++ php/Session_Creation.php
@@ -4,13 +4,13 @@
     $sql_select_id = "SELECT * FROM Room WHERE id=(SELECT MAX(id) FROM Room);";
     $result_id = mysqli_query($conn, $sql_select_id);
     $row_id = mysqli_fetch_assoc($result_id);
-    $_SESSION['id']= $row_id['Session_ID'];
+    $_SESSION['id']= $row_id['SessionID'];
 
     $sql_select = "SELECT * FROM Participant WHERE username='$userName'";
     $result = mysqli_query($conn, $sql_select);
     $row = mysqli_fetch_assoc($result);
 
-    $sql_Connector = "INSERT INTO Session_Guest (UserID, SessionID) VALUES ('12'  ,'12');";
+    $sql_Connector = "INSERT INTO Session_Guest (UserID, SessionID) VALUES ('$row['UserID']'  ,'$_SESSION['id']');";
     mysqli_query($conn, $sql_Connector); 
 
 
