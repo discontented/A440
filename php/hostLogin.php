e22762abf60a10b2fe5bd9893361ca9993f6117a
njfleck <nathanjfleck@hotmail.com>
2018-04-02 21:20:35 -0400

    test
    test

--- php/hostLogin.php
+++ php/hostLogin.php
@@ -19,7 +19,7 @@
         // echo($user);
          $sql_Room = "INSERT INTO Room (time_stamp) VALUES ('NULL');";
          mysqli_query($conn, $sql_Room); 
-         $sql_Connector = "INSERT INTO Session_Guest (UserID, SessionID) VALUES ('$row['UserID']','NULL');";
+         $sql_Connector = "INSERT INTO Session_Guest (UserID) VALUES ('$row['UserID']');";
          mysqli_query($conn, $sql_Connector); 
     //}
     
